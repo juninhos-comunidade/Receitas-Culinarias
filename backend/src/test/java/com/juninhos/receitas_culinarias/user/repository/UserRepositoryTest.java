7204c12ea5581f23758fc3d7b2c639b3e57d9b52
Icaro Torres <98storresm@gmail.com>
2026-07-21 19:04:42 -0300

    feat: Cria entidade, repositório e testes para usuário usando PostgreSQL e TestContainers

--- backend/src/test/java/com/juninhos/receitas_culinarias/user/repository/UserRepositoryTest.java
+++ backend/src/test/java/com/juninhos/receitas_culinarias/user/repository/UserRepositoryTest.java
@@ -3,35 +3,26 @@ package com.juninhos.receitas_culinarias.user.repository;
 import com.juninhos.receitas_culinarias.user.entity.UserEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 @Testcontainers
-@AutoConfigureTesteDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
     @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.10-alpine");
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.username", postgres::getPassword);
-    }
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
     @Autowired
     private UserRepository userRepository;
 
     @Test
-    void shouldSaveAndGetUserSuccessfully() {
+    void deveSalvarEBuscarUsuarioComSucesso() {
         UserEntity user = new UserEntity();
         user.setName("Teste da Silva");
         user.setEmail("teste@comunidade.com");
